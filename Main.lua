2d5563660c4488fce424643ab44c1379f6218039
0x580x540x43 <78674390+0x580x540x43@users.noreply.github.com>
2023-01-08 17:10:21 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -7,7 +7,7 @@ local Scammers = List.Scammers
 local Bots = List.Bots
 
 local Arguments = {...}
-local DisableKey = Arguments.DisableKey or Enum.KeyCode.LeftShift
+local DisableKey = Arguments[1].DisableScriptKey or Enum.KeyCode.LeftShift
 
 print(DisableKey)
 
